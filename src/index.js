f9ba8e4f8f7e3edc7c786ef0faf1c4a239f22373
unknown <almi78@mail.ru>
2021-08-07 13:01:30 +0300

    solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-
 exports.min = function min (array) {
+  if (array && array.length) {
+    array.sort(function(a,b) {return a-b;});
+    return array[0];
+  }
   return 0;
 }
-
 exports.max = function max (array) {
+  if (array && array.length) {
+    array.sort(function(a,b) {return b-a;});
+    return array[0];
+  }
   return 0;
 }
-
 exports.avg = function avg (array) {
+  if (array && array.length) {
+  let summa = array.reduce((sum, current)=>sum+current, 0);
+    return summa/array.length;
+  }
   return 0;
 }
